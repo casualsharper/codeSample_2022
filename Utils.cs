fc11681825a0ac1c81355010776bf85b1082ada7
aleksandrs.jasinskis <aleksandrs.jasinskis.lv@gmail.com>
2022-09-09 08:37:51 +0300

    spacing 2

--- Utils.cs
+++ Utils.cs
@@ -13,10 +13,10 @@ public static class Utils
     public static string ObjectPropertiesValuesToString(object obj)
     {
         return obj.GetType().GetProperties()
-                        .Select(info => (info.Name, Value: info.GetValue(obj, null) ?? "(null)"))
-                        .Aggregate(
-                            new StringBuilder(),
-                            (sb, pair) => sb.AppendLine($"{pair.Name}: {pair.Value}"),
-                            sb => sb.ToString());
+            .Select(info => (info.Name, Value: info.GetValue(obj, null) ?? "(null)"))
+            .Aggregate(
+                new StringBuilder(),
+                (sb, pair) => sb.AppendLine($"{pair.Name}: {pair.Value}"),
+                sb => sb.ToString());
     }
 }
